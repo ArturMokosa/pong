eb4eee8ced052922f0de35d5eb15185d75c041ed
ArturMokosa <30339156+ArturMokosa@users.noreply.github.com>
2017-12-06 09:37:41 +0100

    Initial commit

---
+++ pong/Classes/ClassRacket.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace pong.Classes
+{
+    class ClassRacket : DrawableGameComponent
+    {
+        SpriteBatch spriteBatch;
+        GraphicsDevice graphics;
+        Texture2D pixel;
+
+        int width;
+        int height;
+        public int posY { get; set; }
+        int posX;
+
+        public ClassRacket(GraphicsDevice graphics, SpriteBatch spriteBatch, Game game, int width, int height, int posX,
+            int posY) :
+            base(game)
+        {
+            this.spriteBatch = spriteBatch;
+            this.graphics = graphics;
+            this.posX = posX;
+            this.posY = posY;
+            this.height = height;
+            this.width = width;
+
+            pixel = new Texture2D(graphics, 1, 1);
+
+            pixel.SetData(new Color[] {Color.White});
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            ////posY = Mouse.GetState().Y;
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(pixel, new Rectangle(posX, posY, width, height), Color.White);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ pong/Game1.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using pong.Classes;
+
+namespace pong
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        ClassBall ball;
+        ClassRacket P1;
+        ClassRacket P2;
+
+        int scoreP1 = 0;
+        int scoreP2 = 0;
+        int racketWidht = 10;
+        int racketHeight = 100;
+        int ballSize = 10;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            ball = new ClassBall(GraphicsDevice, spriteBatch, this, ballSize);
+
+            P1 = new ClassRacket(GraphicsDevice, spriteBatch, this, racketWidht, racketHeight, 10,
+                GraphicsDevice.Viewport.Height / 2 - racketHeight / 2);
+            P2 = new ClassRacket(GraphicsDevice, spriteBatch, this, racketWidht, racketHeight,
+                GraphicsDevice.Viewport.Width - 10 - racketWidht,
+                GraphicsDevice.Viewport.Height / 2 - racketHeight / 2);
+
+            Components.Add(P1);
+            Components.Add(P2);
+            Components.Add(ball);
+
+            ball.ResetBall();
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            else if (Keyboard.GetState().IsKeyDown(Keys.Space) && !ball.gameRun)
+            {
+                ball.gameRun = true;
+            }
+
+            P1.posY = Mouse.GetState().Y;
+            P2.posY = Mouse.GetState().Y;
+
+            if (ball.dirX > 0)
+            {
+                ///ball.CheckRacketCollision(P2);
+                if (ball.posY >= P2.posY && ball.posY + ballSize < P2.posY + racketHeight && ball.posX >= P2.posX) //8:52
+                {
+                    
+                }
+            }
+            else if (ball.dirX < 0)
+            {
+                ball.CheckRacketCollision(P1);
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(51, 51, 51));
+
+            base.Draw(gameTime);
+        }
+    }
+}
